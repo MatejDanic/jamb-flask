2841f0c87addfe91ab0a6813597654f594a7f953
Matej Đanić <matej.danic@gmail.com>
2021-01-19 16:40:58 +0100

    potential heroku fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os, random
 app = Flask(__name__)
 app.secret_key = "tanji ključ"
 
-client = MongoClient("mongodb+srv://dbUser:SWDAGlSdPAbgrgEC@cluster0.m0vzw.mongodb.net/jamb?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://dbUser:SWDAGlSdPAbgrgEC@cluster0.m0vzw.mongodb.net/jamb?retryWrites=true&w=majority", connect=False)
 db = client.jamb
 
 
